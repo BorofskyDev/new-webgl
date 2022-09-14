8e7c0fe63e509f0882212b32e0aa51a3d0d753e6
solovyovsdom <joelborofsky@mac.com>
2022-09-13 20:00:29 -0500

    completed faq, responsive

---
+++ src/assets/js/faq.js
@@ -0,0 +1,16 @@
+const questions = [...document.querySelectorAll('.question')];
+questions.map((question) => {
+  let q_text = question.querySelector('h3');
+  question.addEventListener('click', () => {
+    questions
+      .filter((q) => q !== question)
+      .map((q) => q.classList.remove('open'));
+    question.classList.toggle('open');
+  });
+  question.addEventListener('keydown', () => {
+    questions
+      .filter((q) => q !== question)
+      .map((q) => q.classList.remove('open'));
+    question.classList.toggle('open');
+  });
+});
